233e57e9bd24ebff88f7a8edfae873e5b4749774
soneryilgor <113507764+soneryilgor@users.noreply.github.com>
2022-10-10 13:00:14 +0300

    Add files via upload

---
+++ Homeworks/day2Homework/inheritance/src/inheritance/PersonManager.java
@@ -0,0 +1,10 @@
+package inheritance;
+
+public class PersonManager {
+	public void List () {
+		System.out.println("listelendi");
+	}
+	public void Add () {
+		System.out.println("eklendi");
+	}
+}
---
+++ Homeworks/day2Homework/inheritance1/src/inheritance1/CorporateCustomer.java
@@ -0,0 +1,6 @@
+package inheritance1;
+
+public class CorporateCustomer extends Customer{
+	String companyName;
+	String taxNumber;
+}
