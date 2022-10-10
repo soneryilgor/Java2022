d7f14c5030775fa24fca6f756329fe000520e375
soneryilgor <113507764+soneryilgor@users.noreply.github.com>
2022-10-10 12:56:41 +0300

    Add files via upload

---
+++ Homeworks/day1Homework/miniprojeAsalSayi/src/Main.java
@@ -0,0 +1,27 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int number = 1;
+        boolean isPrime = true;
+
+        if (number == 1) {
+            System.out.println("Sayı Asal Değildir!");
+            return;
+        }
+
+        if (number < 1) {
+            System.out.println("Geçersiz Sayı");
+            return;
+        }
+        for (int i = 2; i < number; i++) {
+            if (number % i == 0) {
+                isPrime = false;
+            }
+        }
+        if (isPrime) {
+            System.out.println("Sayı asaldır");
+        } else {
+            System.out.println("Sayı asal değildir");
+        }
+    }
+}
